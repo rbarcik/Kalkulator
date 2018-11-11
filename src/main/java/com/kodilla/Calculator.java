b71124d0927d92ede3acf93f0ebc4a8b9c643a0a
rbarcik <barcik.r@gmail.com>
2018-11-11 23:58:47 +0100

    Commit 3

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,15 @@
 package com.kodilla;
 
 public class Calculator {
+    public int addAToB(int a, int b){
+        return a + b;
+    }
+    public int substractAFromB(int a, int b){
+        return b - a;
+    }
+    public static void main(String args[]) {
+        Calculator calculator = new Calculator();
+        int result = calculator.addAToB(5,5);
+        System.out.println(result);
+    }
 }
